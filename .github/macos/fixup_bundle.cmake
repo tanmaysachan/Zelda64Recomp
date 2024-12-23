2c003605bc6b90bd4631792f423800a65f615737
tanmaysachan <tnmysachan@gmail.com>
2024-12-23 07:46:07 +0530

    Fix "fixup_bundle: not a valid bundle" issue on xcode builds.
    * Xcode app directory was incorrect, it uses the build type as a prefix.

--- .github/macos/fixup_bundle.cmake
+++ .github/macos/fixup_bundle.cmake
@@ -1,8 +1,14 @@
 include(BundleUtilities)
 
+# Xcode generator puts the build type in the build directory
+set(BUILD_PREFIX "")
+if (CMAKE_GENERATOR STREQUAL "Xcode")
+    set(BUILD_PREFIX "${CMAKE_BUILD_TYPE}/")
+endif()
+
 # Use generator expressions to get the absolute path to the bundle and frameworks
-set(APPS "Zelda64Recompiled.app/Contents/MacOS/Zelda64Recompiled")
-set(DIRS "Zelda64Recompiled.app/Contents/Frameworks")
+set(APPS "${BUILD_PREFIX}Zelda64Recompiled.app/Contents/MacOS/Zelda64Recompiled")
+set(DIRS "${BUILD_PREFIX}Zelda64Recompiled.app/Contents/Frameworks")
 
 # The fixup_bundle command needs an absolute path
 file(REAL_PATH ${APPS} APPS)
